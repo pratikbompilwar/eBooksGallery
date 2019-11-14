d78fd98d9e0e2810390ea10e0ba9e7b3757bb35a
Pratik Bompilwar <49276@hexaware.com>
2019-11-14 17:19:58 +0530

    added insert update delete find byID

--- BooksCatalogAPI/BooksCatalogAPI/Controllers/BooksCatalogController.cs
+++ BooksCatalogAPI/BooksCatalogAPI/Controllers/BooksCatalogController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using BooksCatalogAPI.Helpers;
 using BooksCatalogAPI.Infra;
 using BooksCatalogAPI.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -9,6 +12,7 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace BooksCatalogAPI.Controllers
@@ -32,21 +36,152 @@ namespace BooksCatalogAPI.Controllers
         public async Task<ActionResult<List<Book>>> GetBooks()
         {
             var result = await this.dbContext.BooksCatalog.FindAsync<Book>(FilterDefinition<Book>.Empty);
-            return result.ToList();
-            //var result = (await this.dbContext.BooksCatalog.FindAsync<Book>(FilterDefinition<Book>.Empty)).ToList();
-            //return result;
+            return result.ToList();           
         }
 
-        [HttpPost("book", Name = "AddBook")]
+        [HttpPost("addbook", Name = "AddBook")]
         public ActionResult<Book> AddBook()
-        {            
+        {
+            // var coverImageName = SaveImageToCloudAsync(Request.Form.Files[0]).GetAwaiter().GetResult();
+
+            //var book = new Book()
+            //{
+            //    Title = Request.Form["title"],               
+            //   SubTitle = Request.Form["subTitle"],
+            //   Author= Request.Form["author"],
+            //   Description= Request.Form["description"],
+            //   Price= Double.Parse(Request.Form["price"]),
+            //   Quantity = Int32.Parse(Request.Form["quantity"]),
+            //   Language= Request.Form["language"],
+            //   CoverImageUrl= coverImageName,
+            //   CreatedDate= DateTime.Parse(Request.Form["createdDate"]),
+            //   UpdatedDate= DateTime.Parse(Request.Form["updatedDate"]),
+            //   isFree= Boolean.Parse(Request.Form["isFree"]),
+            //   isAvailable= Boolean.Parse(Request.Form["isAvailable"]),
+            //   Status= Request.Form["status"],
+            //   BookLocationPath= Request.Form["bookLocationPath"]
+            //};
 
             var book = new Book()
             {
-                Title = "MyBook"                
+                Title = "MyBooks"
             };
             dbContext.BooksCatalog.InsertOne(book);  // saving to mongo            
             return book;
+        }
+
+        [HttpPost("updatebook", Name = "UpdateBook")]
+        public ActionResult<Book> UpdateBook()
+        {
+            var coverImageName = SaveImageToCloudAsync(Request.Form.Files[0]).GetAwaiter().GetResult();
+
+            FilterDefinition<Book> filter = "{ Id:"+ Request.Form["id"] + " }";
+
+            //UpdateDefinition<BsonDocument> update = "{ $set: { x: 1 } }";
+
+            var bookId = Request.Form["id"];
+
+            var book = new Book()
+            {
+                Title = Request.Form["title"],
+                SubTitle = Request.Form["subTitle"],
+                Author = Request.Form["author"],
+                Description = Request.Form["description"],
+                Price = Double.Parse(Request.Form["price"]),
+                Quantity = Int32.Parse(Request.Form["quantity"]),
+                Language = Request.Form["language"],
+                CoverImageUrl = coverImageName,
+                CreatedDate = DateTime.Parse(Request.Form["createdDate"]),
+                UpdatedDate = DateTime.Parse(Request.Form["updatedDate"]),
+                isFree = Boolean.Parse(Request.Form["isFree"]),
+                isAvailable = Boolean.Parse(Request.Form["isAvailable"]),
+                Status = Request.Form["status"],
+                BookLocationPath = Request.Form["bookLocationPath"]
+            };
+            //dbContext.BooksCatalog.UpdateOne(filter, book);  // saving to mongo     
+            dbContext.BooksCatalog.ReplaceOne(x=>x.Id== bookId,book);
+            return book;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}", Name = "GetBookById")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Book>> GetBookById(string id)
+        {
+
+            var builder = Builders<Book>.Filter;
+
+            var filter = builder.Eq("Id", id);
+
+            var result = await dbContext.BooksCatalog.FindAsync(filter);
+
+            var item = result.FirstOrDefault();
+
+            if (item == null)
+
+            {
+
+                return NotFound(); //Not found , Status code 404
+
+            }
+
+            else
+
+            {
+
+                return Ok(item); //Found , status code 200
+
+            }
+
+        }
+
+        [AllowAnonymous]
+        [HttpDelete("DeleteBook/{id}", Name = "DeleteById")]
+        public string DeleteById(string id)
+
+        {
+
+            var result = this.dbContext.BooksCatalog.DeleteOne(book => book.Id == id);
+
+            if (result.DeletedCount > 0)
+
+            {
+
+                return "Deleted";
+
+            }
+
+            else
+
+            {
+
+                return "";
+
+            }
+
+        }
+
+        [NonAction]
+        private async Task<string> SaveImageToCloudAsync(IFormFile image)
+        {
+            var imageName = $"{Guid.NewGuid()}_{Request.Form.Files[0].FileName}";
+            var tempFile = Path.GetTempFileName();
+            using (FileStream fs = new FileStream(tempFile, FileMode.Create))
+            {
+                await image.CopyToAsync(fs);
+            }
+
+            var imageFile = Path.Combine(Path.GetDirectoryName(tempFile), imageName);
+            System.IO.File.Move(tempFile, imageFile);
+            StorageAccountHelper storageHelper = new StorageAccountHelper();
+            storageHelper.StorageConnectionString = config.GetConnectionString("StorageConnection");
+            var fileUri = await storageHelper.UploadFileToBlobAsync(imageFile, "ebooksgalleryimages");
+
+            System.IO.File.Delete(imageFile);
+
+            //fileUri = fileUri + "?sv=2019-02-02&ss=bfq&srt=sco&sp=rwdlacup&se=2019-11-15T15:22:16Z&st=2019-11-06T07:22:16Z&spr=https&sig=Bshl%2Bplh44Wu3w4e8XIGemZgLhamR%2FrFXdDvIqJdkbg%3D";
+            return fileUri;
         }
     }
 }
---
+++ BooksCatalogAPI/BooksCatalogAPI/Helpers/StorageAccountHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksCatalogAPI.Helpers
+{   
+    public class StorageAccountHelper
+    {
+        public string storageConnectionString;
+        public string tableConnectionString;
+        private CloudStorageAccount storageAccount;
+        private CloudStorageAccount tableStorageAccount;
+        private CloudBlobClient blobClient;
+        private CloudTableClient tableClient;
+
+        public string StorageConnectionString
+        {
+            get { return storageConnectionString; }
+            set
+            {
+                this.storageConnectionString = value;
+                storageAccount = CloudStorageAccount.Parse(this.storageConnectionString);
+            }
+        }
+       
+
+        public async Task<string> UploadFileToBlobAsync(string filePath, string containerName)
+        {
+            blobClient = storageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(containerName);
+            await container.CreateIfNotExistsAsync();
+            BlobContainerPermissions permissions = new BlobContainerPermissions()
+            {
+                PublicAccess = BlobContainerPublicAccessType.Container
+            };
+            await container.SetPermissionsAsync(permissions);
+
+            var fileName = Path.GetFileName(filePath);
+            var blob = container.GetBlockBlobReference(fileName);
+            await blob.DeleteIfExistsAsync();
+
+            await blob.UploadFromFileAsync(filePath);
+            return blob.Uri.AbsoluteUri;
+
+        }        
+    }
+}
